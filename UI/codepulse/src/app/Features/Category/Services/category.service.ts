2e1a9ba26225e703abeee15cbb9eea33e57a5d14
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 18:45:57 +0100

    Added getcategoryById Service

--- UI/codepulse/src/app/Features/Category/Services/category.service.ts
+++ UI/codepulse/src/app/Features/Category/Services/category.service.ts
@@ -20,4 +20,8 @@ export class CategoryService {
   getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   }
+
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
 }
