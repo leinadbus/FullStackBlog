f7494fcbd44bfc94ce8f66042bc4267fbc35356e
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 19:04:49 +0100

    Added EditCategory method to CategoriesController

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -83,5 +83,36 @@ namespace CodePulse.API.Controllers
             };
             return Ok(response);
         }
+
+        //PUT: /api/categories/{id}
+        [HttpPut]
+        [Route("edit/{id:Guid")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            //Convert DTO to Domain Model
+            var category = new Category()
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await categoryRepository.UpdateAsync(category);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            //Convert Domain model to DTO
+            var response = new CategoryDto()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
