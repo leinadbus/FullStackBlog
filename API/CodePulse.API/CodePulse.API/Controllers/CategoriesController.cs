80cca6d0d97937aa9ed33a789d496086766dc848
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 10:25:36 +0100

    Added CategoriesController

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ApplicationDbContext dbContext; 
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        
+    }
+}
