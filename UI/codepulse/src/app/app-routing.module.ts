3e82ac2421946c3f5228b93813269d33c0ee3232
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 12:02:35 +0100

    Added AddCategoryComponent routing

--- UI/codepulse/src/app/app-routing.module.ts
+++ UI/codepulse/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
+import { AddCategoryComponent } from './Features/Category/add-category/add-category.component';
 
 const routes: Routes = [
   {
     path: 'admin/categories',
     component: CategoryListComponent
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent
   }
 ];
 
