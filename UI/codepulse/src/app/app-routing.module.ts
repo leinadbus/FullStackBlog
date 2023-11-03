52594532bc0bf44609c833a8e550cd49d6507656
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 18:26:59 +0100

    Added routing with a button in category-list component

--- UI/codepulse/src/app/app-routing.module.ts
+++ UI/codepulse/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
 import { AddCategoryComponent } from './Features/Category/add-category/add-category.component';
+import { EditCategoryComponent } from './Features/Category/edit-category/edit-category.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'admin/categories/add',
     component: AddCategoryComponent
+  },
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent
   }
 ];
 
