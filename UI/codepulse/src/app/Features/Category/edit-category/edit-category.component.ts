4459e61a2d2dfde34ffcaeb75a562b1f82a0adaf
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 19:21:40 +0100

    Added on UI a refactor for UpdateCategoryById

--- UI/codepulse/src/app/Features/Category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/Features/Category/edit-category/edit-category.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../Services/category.service';
+import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -12,9 +15,12 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
 
   id: string | null = null;
   paramsSubscription?: Subscription;
+  categorySubscription?: Subscription;
+  category?: Category;
 
-
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private router: Router) {
 
   }
 
@@ -22,12 +28,40 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
     this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if (this.id) {
+          //Get the data from the API for this category ID
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next: (response) => {
+              this.category = response;
+            }
+          });
+        }
       }
     });
   }
 
+  onFormSubmit(): void {
+    const updateCategoryRequest: UpdateCategoryRequest = {
+      name: this.category?.name ?? '',
+      urlHandle: this.category?.urlHandle ?? ''
+    };
+
+    //Pass this object to service
+    if(this.id){
+      this.categorySubscription = this.categoryService.updateCategory(this.id,updateCategoryRequest)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/categories');
+        }
+      });
+    }
+  }
+
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
+    this.categorySubscription?.unsubscribe();
   }
 
 }
