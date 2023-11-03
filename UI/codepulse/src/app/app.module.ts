5e1fc08b1a12296a4b540138c9a5ec8f29037134
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 12:21:38 +0100

    Imported HttpClientModule from @angular/common/http

--- UI/codepulse/src/app/app.module.ts
+++ UI/codepulse/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './Core/Components/navbar/navbar.component';
 import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
 import { AddCategoryComponent } from './Features/Category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
