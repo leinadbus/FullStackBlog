90fd74e3cda02068ecd355e0278c753f1b52ba8b
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-11-03 10:49:30 +0100

    Inyected <ICategoryRepository,CategoryRepository> in Program

--- API/CodePulse.API/CodePulse.API/Program.cs
+++ API/CodePulse.API/CodePulse.API/Program.cs
@@ -1,4 +1,6 @@
 using CodePulse.API.Data;
+using CodePulse.API.Repositories.Implementation;
+using CodePulse.API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
 });
 
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 
 var app = builder.Build();
 
